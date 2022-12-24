f76f9cdc4e93ba631c60b5e7b6029fb2e277a3eb
Jasmine S <jasspears@gmail.com>
2022-12-24 17:47:54 -0600

    added todo
    add to-dos

--- Codecademy/back-end/number-guesser/script.js
+++ Codecademy/back-end/number-guesser/script.js
@@ -2,18 +2,19 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// TODO: 
+// - add check to prevent user input below 0 or above 9
+// - refactor: make function that returns the abs values and then 
+// integrate into the if statement
+// - maybe end the game, disable input and declare game winner after so many rounds
 
 const generateTarget = () => Math.floor(Math.random() * 10);
 const compareGuesses = (userGuess, compGuess, target) => {
   if (Math.abs(target - userGuess) === Math.abs(target - compGuess)) {
-    console.log('Tied. User wins.');
     return true;
   } else if (Math.abs(target - userGuess) < Math.abs(target - compGuess)) {
-    console.log('User wins.');
     return true;  
   }
-  console.log('Computer wins.');
   return false;
 }
 
