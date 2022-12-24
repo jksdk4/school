b55e298bb4e5026f2d41cf64f98e09a276aaeb20
Jasmine S <jasspears@gmail.com>
2022-12-24 17:20:06 -0600

    added basic solution
    Basic solution to make game functional.

--- Codecademy/back-end/number-guesser/script.js
+++ Codecademy/back-end/number-guesser/script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+const compareGuesses = (userGuess, compGuess, target) => {
+  if (Math.abs(target - userGuess) === Math.abs(target - compGuess)) {
+    console.log('Tied. User wins.');
+    return true;
+  } else if (Math.abs(target - userGuess) < Math.abs(target - compGuess)) {
+    console.log('User wins.');
+    return true;  
+  }
+  console.log('Computer wins.');
+  return false;
+}
+
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
